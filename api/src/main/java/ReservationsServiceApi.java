dc1599ca1ffc267431c093ae579537eb3a2f6894
b0gdanp3trovic <bogdanlmaorofl@gmail.com>
2021-12-31 05:46:13 +0100

    Add OpenAPI

--- api/src/main/java/ReservationsServiceApi.java
+++ api/src/main/java/ReservationsServiceApi.java
@@ -1,6 +1,13 @@
 import beans.ReservationsBean;
 import classes.Reservation;
 import com.kumuluz.ee.logs.cdi.Log;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.headers.Header;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import restclients.ListingsApiClient;
 import restclients.interfaces.ListingsApi;
 
@@ -23,6 +30,13 @@ public class ReservationsServiceApi {
     @Inject
     private ListingsApiClient listingsApiClient;
 
+    @Operation(description = "Get all reservations.", summary = "Get all reservations.")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "List of all reservations",
+                    content = @Content(schema = @Schema(implementation = Reservation.class, type = SchemaType.ARRAY)),
+                    headers = {@Header(name = "X-Total-Count", description = "Number of objects in list")}
+            )})
     @GET
     public Response getAllReservations() {
         List<Reservation> reservation = reservationsBean.getReservations();
@@ -30,6 +44,13 @@ public class ReservationsServiceApi {
 
     }
 
+    @Operation(description = "Get a listing by id.", summary = "Get listing by id")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "Get listing by id",
+                    content = @Content(
+                            schema = @Schema(implementation = Reservation.class))
+            )})
     @GET
     @Path("/{reservationId}")
     public Response getReservationById(@PathParam("reservationId") Integer reservationId) {
@@ -37,6 +58,13 @@ public class ReservationsServiceApi {
         return Response.status(Response.Status.OK).entity(reservation).build();
     }
 
+    @Operation(description = "Add reservation.", summary = "Add reservation")
+    @APIResponses({
+            @APIResponse(responseCode = "201",
+                    description = "Reservation successfully added."
+            ),
+            @APIResponse(responseCode = "400", description = "Validation error. Check request parameters")
+    })
     @POST
     public Response createReservation(Reservation reservation) {
         if(reservation.getOwnerId() == null || reservation.getReserverId() == null || reservation.getStartDate() == null
--- api/src/main/java/application/ReservationsServiceApplication.java
+++ api/src/main/java/application/ReservationsServiceApplication.java
@@ -1,8 +1,18 @@
 package application;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+@OpenAPIDefinition(info = @Info(title = "Listings API", version = "v1",
+        contact = @Contact(email = "bp3201@student.uni-lj.si"),
+        license = @License(name = "dev"), description = "API for managing reservations."),
+        servers = @Server(url = "http://206.189.249.190/reservations-service/"))
 @ApplicationPath("v1")
 public class ReservationsServiceApplication extends Application {
 }
