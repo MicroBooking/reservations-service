5c51f08336d7ed8626cc90a722c0984083375b91
b0gdanp3trovic <bogdanlmaorofl@gmail.com>
2022-01-13 21:54:04 +0100

    update health check

---
+++ api/src/main/java/health/ListingsServiceReachableHealthCheck.java
@@ -0,0 +1,26 @@
+package health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+import restclients.ListingsApiClient;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@Liveness
+@ApplicationScoped
+public class ListingsServiceReachableHealthCheck implements HealthCheck {
+    @Inject
+    private ListingsApiClient listingsApiClient;
+
+    @Override
+    public HealthCheckResponse call() {
+        try {
+            listingsApiClient.getListings();
+            return HealthCheckResponse.up(ListingsServiceReachableHealthCheck.class.getSimpleName());
+        } catch (Exception e){
+            return HealthCheckResponse.down(ListingsServiceReachableHealthCheck.class.getSimpleName());
+        }
+    }
+}
